087c8fde17e99f100e0fc326fe79c3ff472d1a61
happamaru <tubotubo1182@gmail.com>
2023-09-19 08:12:25 +0900

    カメラ操作作成

---
+++ Assets/Teruyama/Scripts/Camera/CameraManager.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraManager : MonoBehaviour
+{
+   [SerializeField] GameObject player;
+   [SerializeField] float LimitLeftPosX;
+   [SerializeField] float LimitRightPosX;
+    private void LateUpdate() {
+        this.transform.position = new Vector3(player.transform.position.x,player.transform.position.y,-10);
+
+        if(this.transform.position.x < LimitLeftPosX){
+            this.transform.SetPosX(LimitLeftPosX);
+        }
+        if(this.transform.position.x > LimitRightPosX){
+            this.transform.SetPosX(LimitRightPosX);
+        }        
+        if(this.transform.position.y < 0){
+            this.transform.SetPosY(0);
+        }
+   }
+}
--- Assets/Teruyama/Scripts/Player/PlayerCommand.cs
+++ Assets/Teruyama/Scripts/Player/PlayerCommand.cs
@@ -4,11 +4,9 @@ using UnityEngine;
 
 public class HighJump:ICommand{
     public IEnumerator Command(PlayerController player){
-      player.MaxSpeed = 15;
+     
       var rg2d = player.rg2d;
-      rg2d.velocity = Vector2.zero;
-      rg2d.gravityScale = 0;
-      rg2d.AddForce(new Vector2(player._inputX,0) * 1000);
+      rg2d.AddForce(Vector2.up * 1000);
       yield return new WaitForSeconds(0.3f);
     }
 }
@@ -18,7 +16,7 @@ public class HighSpeed:ICommand{
       var rg2d = player.rg2d;
       rg2d.velocity = Vector2.zero;
       rg2d.gravityScale = 0;
-      rg2d.AddForce(new Vector2(player._inputX,0) * 1000);
+      rg2d.AddForce(new Vector2(player.gameObject.transform.localScale.x,0) * 1000);
       yield return new WaitForSeconds(0.3f);
     }
 }
