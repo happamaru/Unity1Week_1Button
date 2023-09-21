79c3e99e430eadb3e42a4c0ab5e6daf17aab2d3c
happamaru <tubotubo1182@gmail.com>
2023-09-21 13:52:05 +0900

    IScoreの追加

---
+++ Assets/Takasu/Scripts/IScore.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IScore
+{
+    /// <summary>
+    /// returnでアイテムのスコアを返すようにしてほしい
+    /// </summary>
+    /// <returns></returns>
+   int AddScore();
+}
