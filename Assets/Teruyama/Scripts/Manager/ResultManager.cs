f82db056e56f7463b6fd0ebc10e224c42589a567
happamaru <tubotubo1182@gmail.com>
2023-09-22 14:46:48 +0900

    リザルトそれっぽく

--- Assets/Teruyama/Scripts/Manager/ResultManager.cs
+++ Assets/Teruyama/Scripts/Manager/ResultManager.cs
@@ -7,8 +7,10 @@ using UnityEngine.SceneManagement;
 public class ResultManager : MonoBehaviour
 {
     [SerializeField] TextMeshProUGUI ResultScore; 
+    [SerializeField] ButtonManager buttonManager;
     void Start(){
         ResultScore.text = GameManager.resultScore.ToString();
+        buttonManager.onClickCallback = ToTitle;
     }
     public void ToTitle(){
         SceneManager.LoadScene("TitleScene");
