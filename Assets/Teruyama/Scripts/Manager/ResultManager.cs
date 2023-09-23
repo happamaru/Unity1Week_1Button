9df9002fd24ae8622076de7dbcf003bbeda1e8a8
happamaru <tubotubo1182@gmail.com>
2023-09-23 14:34:26 +0900

    オンラインランキング作成

--- Assets/Teruyama/Scripts/Manager/ResultManager.cs
+++ Assets/Teruyama/Scripts/Manager/ResultManager.cs
@@ -8,11 +8,23 @@ public class ResultManager : MonoBehaviour
 {
     [SerializeField] TextMeshProUGUI ResultScore; 
     [SerializeField] ButtonManager buttonManager;
+     [SerializeField] ButtonManager RankingButton;
+     
+     [SerializeField] bool IsDebug;
+     [SerializeField] int DebugScore;
     void Start(){
         ResultScore.text = GameManager.resultScore.ToString();
         buttonManager.onClickCallback = ToTitle;
+        RankingButton.onClickCallback = OpenRanking;
     }
     public void ToTitle(){
         SceneManager.LoadScene("TitleScene");
+    }
+    public void OpenRanking(){
+        if(IsDebug){
+            naichilab.RankingLoader.Instance.SendScoreAndShowRanking(DebugScore);    
+        }else{
+        naichilab.RankingLoader.Instance.SendScoreAndShowRanking (GameManager.resultScore);
+        }
     }
 }
